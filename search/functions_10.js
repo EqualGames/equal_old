72708f7f95ecdc92f41b70755941ff56edd04683
Equal <6087948+is-equal@users.noreply.github.com>
2019-08-16 13:15:53 -0300

    Updates

--- search/functions_10.js
+++ search/functions_10.js
@@ -6,7 +6,7 @@ var searchData=
   ['red',['RED',['../namespaceeq_1_1out.html#ac7f20514e59ceff3b85b8ea2a444cfcc',1,'eq::out']]],
   ['register_5fevent',['register_event',['../classeq_1_1Script.html#ab7938a842e3110b022a13b4efff40839',1,'eq::Script']]],
   ['registeraction',['registerAction',['../classeq_1_1ButtonScript.html#a7cf04bd7f8a0707993eed64a278864f8',1,'eq::ButtonScript']]],
-  ['remove',['remove',['../classeq_1_1TransformComponent.html#a15481a6686153873f7c4d6dfb0d250ea',1,'eq::TransformComponent']]],
+  ['remove',['remove',['../classeq_1_1TransformComponent.html#a6e989445aefdd50b0c2ca2f9d445d1cd',1,'eq::TransformComponent']]],
   ['renderercomponent',['RendererComponent',['../classeq_1_1RendererComponent.html#a7d6759de79073cc7db8b3ac8af8226fc',1,'eq::RendererComponent::RendererComponent()=default'],['../classeq_1_1RendererComponent.html#a66aec508ed475ad901bb28ca3a49b1e4',1,'eq::RendererComponent::RendererComponent(Texture *texture)']]],
   ['replace',['replace',['../group__str.html#ga2c100ac4bbb9387d784e34defe700c85',1,'eq::str']]],
   ['reset',['reset',['../classeq_1_1RendererComponent.html#afbdbe2ddd1f78de0c80de136668e2bde',1,'eq::RendererComponent::reset()'],['../classeq_1_1Timestep.html#aa8c7e5e281e65d1120beec58bcdf1156',1,'eq::Timestep::reset()']]],
